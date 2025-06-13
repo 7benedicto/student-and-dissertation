faf2a94ecd2268e573200baacdeeef772723f71b
Benedicto <bisakalaben03@gmail.com>
2025-06-12 17:38:46 -0700

    changes

--- dissertation_project/urls.py
+++ dissertation_project/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('student_dissertation.urls')),
-#     path('', RedirectView.as_view(url='https://react-dissertation.vercel.app/', permanent=True)),  # Redirect root to React frontend
+    path('', RedirectView.as_view(url='https://react-dissertation.vercel.app/', permanent=True)),  # Redirect root to React frontend
 ]
 
 if settings.DEBUG:
